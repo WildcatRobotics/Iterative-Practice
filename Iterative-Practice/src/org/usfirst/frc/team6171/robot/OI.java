f54eee0321e3f86f34d79db8d9fbc0c19112a286
Carson <carson.schubert14@gmail.com>
2015-12-28 11:13:26 -0600

    Hotfix

--- Iterative-Practice/src/org/usfirst/frc/team6171/robot/OI.java
+++ Iterative-Practice/src/org/usfirst/frc/team6171/robot/OI.java
@@ -1,12 +1,21 @@
 package org.usfirst.frc.team6171.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class OI {
 	
 	Joystick joy;
+	JoystickButton X;
+	JoystickButton A;
+	JoystickButton B;
+	JoystickButton Y;
 	
 	public OI(){
 		joy = new Joystick(0);
+		X = new JoystickButton(joy, 1);
+		A = new JoystickButton(joy, 2);
+		B = new JoystickButton(joy, 3);
+		Y = new JoystickButton(joy, 4);
 	}
 }
--- Iterative-Practice/src/org/usfirst/frc/team6171/robot/Robot.java
+++ Iterative-Practice/src/org/usfirst/frc/team6171/robot/Robot.java
@@ -3,7 +3,9 @@ package org.usfirst.frc.team6171.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -39,7 +41,7 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
+    	leftFront.set(1);
     }
 
     /**
@@ -49,6 +51,12 @@ public class Robot extends IterativeRobot {
     	SmartDashboard.putDouble("Joystick", oi.joy.getRawAxis(1));
     	SmartDashboard.putData("Motor", leftFront);
     	leftFront.set(oi.joy.getRawAxis(1));
+    	if(oi.X.get()){
+    		leftFront.set(1);
+    	}
+    	if(oi.B.get()){
+    		leftFront.set(-1);
+    	}
     	
     	//drive.setMaxOutput(.5);
         //drive.tankDrive(oi.joy.getRawAxis(1), oi.joy.getRawAxis(3));
@@ -58,7 +66,7 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-    
+    	LiveWindow.addActuator("LeftFront", 0, leftFront);
     }
     
 }
